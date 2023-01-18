e9e0c536b7a8d3cdd42e5b636449418a68a29af3
Mike Lake <MikeL@speleonics.com.au>
2023-01-18 22:59:09 +1100

    Added make script and updated CI

---
+++ make_html.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script to generate HTML page from the Markdown document.
+
+input="mfield_data/epa_mfield_2002_utf8_short.md"
+styles="assets/styles.css"
+header="assets/header.html"
+footer="assets/footer.html"
+output="epa_mfield_2002_utf8_short.html"
+tmp="tmp.txt"
+
+today=$(date +"%A, %d %B %Y")
+
+cat $header > $output
+echo '<style type="text/css">' >> $output
+cat $styles >> $output
+echo '</style>' >> $output
+cat assets/start.html | sed "s/UPDATE_DATE/$today/" >> $output
+
+pandoc $input --columns=10000 > $tmp
+# Replace line like this: <td>7</td>
+# With lines like this:   <td id="7">7</td>
+cat $tmp | sed 's/<td>\([0-9]*\)<\/td>/<td id="\1">\1<\/td>/' >> $output
+
+cat $footer >> $output
+rm -f $tmp
+
